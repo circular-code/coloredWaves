50a9ddb11e3b0b5004b12545d8fbec5145a0c3bb
circular-code <stephanos@malikotinas.de>
2020-02-23 22:48:44 +0100

    added gui sliders

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-'use strict';
-
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function getRandomRGB(r,g,b) {
    return 'rgba(' + Math.random() * r + ',' + Math.random() * g + ',' + Math.random() * b + ',0.3)';
+}
+
+var gui = {
+    amplitude: Math.random() * 200,
+    waveLength: Math.random() / 100,
+    frequency: Math.random() * 1,
 };
 
+document.getElementById('amplitude').addEventListener('change', function(e){
+    gui.amplitude = Math.random() * +e.srcElement.value;
+});
+
+document.getElementById('frequency').addEventListener('change', function(e){
+    gui.frequency = Math.random() * +e.srcElement.value / 10;
+});
+
+document.getElementById('waveLength').addEventListener('change', function(e){
+    gui.waveLength = Math.random() / +e.srcElement.value;
+});
+
 function runAnimation(target) {
+    'use strict';
+
     var canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
     target.append(canvas);
     canvas.width = target.clientWidth;
     canvas.height = target.clientHeight;
-    let size = (target.clientWidth + target.clientHeight) / 2;
-    canvas.addEventListener("click", mouseClickHandler, false);
-    canvas.addEventListener("touchstart", mouseClickHandler, false);
+    ctx.lineWidth = 2;
 
     // adjust to vertical scrollbar
     var width = window.document.body.clientWidth < canvas.width ? window.document.body.clientWidth : canvas.width;
-
-    function mouseClickHandler(e) {
-
-    }
 
     window.addEventListener('resize', function() {
         canvas.width = target.clientWidth;
@@ -43,43 +55,37 @@ function runAnimation(target) {
 
         draw () {
             ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
+            ctx.moveTo(this.x, this.y + Math.sin(this.x * (this.waveLength + gui.waveLength) + this.moveBy) * (this.amplitude + gui.amplitude) * Math.sin(this.moveBy));
 
             for (let i = 0; i < width; i++)
-                ctx.lineTo(i, this.y + Math.sin(i * this.waveLength + this.moveBy) * this.amplitude * Math.sin(this.moveBy));
-
-            ctx.moveTo(this.x, this.y);
+                ctx.lineTo(i, this.y + Math.sin(i * (this.waveLength + gui.waveLength) + this.moveBy) * (this.amplitude + gui.amplitude) * Math.sin(this.moveBy));
 
             ctx.strokeStyle = this.color;
-            ctx.lineWidth = 3;
             ctx.stroke();
         }
 
         move (timeDelta) {
-            this.moveBy += this.frequency * timeDelta;
+            this.moveBy += (this.frequency + gui.frequency) * timeDelta;
         }
     }
 
     var waves = [];
-    for (let i = 0, amount = 6; i < amount; i++)
-        waves.push(new Wave(0, canvas.height/2, Math.random() / 100, Math.random() * 300, getRandomRGB(255,0,125) , 0.2));
+    for (let i = 0, amount = 10; i < amount; i++)
+        waves.push(new Wave(0, canvas.height/2, Math.random() / 250, Math.random() * 200, getRandomRGB(255,0,125) , Math.random()));
 
     var time = Date.now();
 
-    function renderLoop(now) {
+    function renderLoop(now = Date.now()) {
 
         var timeDelta = (now - time) / 1000;
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         //prevent huge jumps of dot movement when leaving site
-        if (timeDelta >= 0.05)
+        if (timeDelta >= 0.05 || timeDelta <= 0)
             timeDelta = 0.01;
 
-        waves.forEach(wave => {
-            wave.move(timeDelta || 0);
-            wave.draw();
-        });
+        waves.forEach(wave => (wave.move(timeDelta), wave.draw()));
 
         time = now;
 
